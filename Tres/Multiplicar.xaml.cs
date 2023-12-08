71ecfc33e4283924a829bde4255ae3ef8f5a9ecc
Gaspar <e972136@gmail.com>
2023-12-07 23:42:49 -0600

    Trycatch

--- Tres/Multiplicar.xaml.cs
+++ Tres/Multiplicar.xaml.cs
@@ -9,10 +9,17 @@ public partial class Multiplicar : ContentPage
 
     private void OnButtonClicked(object sender, EventArgs e)
     {
-        var dato1 = double.Parse(Dato1.Text);
-        var dato2 = double.Parse(Dato2.Text);
-        var dato3 = double.Parse(Dato3.Text);
-        var resultado = dato1 * dato2 * dato3;
-        Respuesta.Text = resultado.ToString();
+        try
+        {
+            var dato1 = double.Parse(Dato1.Text);
+            var dato2 = double.Parse(Dato2.Text);
+            var dato3 = double.Parse(Dato3.Text);
+            var resultado = dato1 * dato2 * dato3;
+            Respuesta.Text = resultado.ToString();
+        }catch(Exception ex)
+        {
+            DisplayAlert("Error", ex.Message, "ok");
+        }
+
     }
 }
