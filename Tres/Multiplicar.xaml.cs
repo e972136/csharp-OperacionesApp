94b464c92ddaa2521268a747d992d22b993d2acd
Gaspar <e972136@gmail.com>
2023-12-07 22:59:15 -0600

    Multiplicacion

--- Tres/Multiplicar.xaml.cs
+++ Tres/Multiplicar.xaml.cs
@@ -6,4 +6,13 @@ public partial class Multiplicar : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void OnButtonClicked(object sender, EventArgs e)
+    {
+        var dato1 = double.Parse(Dato1.Text);
+        var dato2 = double.Parse(Dato2.Text);
+        var dato3 = double.Parse(Dato3.Text);
+        var resultado = dato1 * dato2 * dato3;
+        Respuesta.Text = resultado.ToString();
+    }
 }
